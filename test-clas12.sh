aa378705ee8ea511b64fbd0b8cf7dc6a701f0d31
Nathan Baltzell <baltzell@gmx.com>
2024-04-03 15:16:53 -0400

    add an sqlite test

--- test-clas12.sh
+++ test-clas12.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export JAVA_OPTS='-Djava.io.tmpdir=. -Dsqlite.io.tmpdir=.'
+
 groovy=$(cat <<EOF
 import org.jlab.ccdb.CCDB;
 import org.rcdb.RCDB;
@@ -16,5 +18,25 @@ System.out.println(System.currentTimeMillis()-startTime);
 EOF
 )
 
-groovy -cp ./target/ccdbrcdb-1.0-SNAPSHOT-with-dependencies.jar -e " $groovy "
+echo Checking clasdb.jlab.org ...
+groovy -cp ./target/ccdbrcdb-*-with-dependencies.jar -e " $groovy "
+
+groovy=$(cat <<EOF
+import org.jlab.ccdb.CCDB;
+import org.rcdb.RCDB;
+int run=4013;
+ccdb = CCDB.createProvider("sqlite:////cvmfs/oasis.opensciencegrid.org/jlab/hallb/clas12/sw/noarch/data/ccdb/ccdb_latest.sqlite");
+rcdb = RCDB.createProvider("sqlite:////cvmfs/oasis.opensciencegrid.org/jlab/hallb/clas12/sw/noarch/data/rcdb/rcdb_latest.sqlite");
+ccdb.setDefaultRun(run);
+ccdb.connect();
+rcdb.connect();
+System.out.println("RCDB: "+rcdb.getCondition(run,"run_start_time").toTime());
+startTime = System.currentTimeMillis();
+System.out.println("CCDB: "+ccdb.getData("/calibration/ec/tmf_window").getBlob());
+System.out.println(System.currentTimeMillis()-startTime);
+EOF
+)
+
+echo Checking sqlite ...
+groovy -cp ./target/ccdbrcdb-*-with-dependencies.jar -e " $groovy "
 
